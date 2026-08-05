040801e9eb34418b7730e6282559c728802674f8
ansycreator <mdnasimali8697255941@gmail.com>
2026-08-05 08:24:05 +0530

    Add files via upload

---
+++ scriptttt.js
@@ -0,0 +1,52 @@
+const count = document.getElementById("count");
+const countdown = document.getElementById("countdown");
+const birthday = document.getElementById("birthday");
+const typing = document.getElementById("typing");
+const surpriseBtn = document.getElementById("surpriseBtn");
+const surprise = document.getElementById("surprise");
+
+let number = 3;
+
+const timer = setInterval(() => {
+    number--;
+
+    if (number > 0) {
+        count.innerHTML = number;
+    } else {
+        clearInterval(timer);
+
+        countdown.classList.add("hidden");
+        birthday.classList.remove("hidden");
+
+        typeMessage();
+    }
+}, 1000);
+
+const message =
+`Happy Birthday, Sohana! 🎂❤️
+
+May Allah bless you with happiness, peace, good health, success, and countless beautiful moments.
+
+May your smile always shine brightly, and may every good dream in your heart come true.
+
+Have a wonderful birthday and a fantastic year ahead! 🌸✨`;
+
+let index = 0;
+
+function typeMessage() {
+
+    if (index < message.length) {
+        typing.innerHTML += message.charAt(index);
+        index++;
+        setTimeout(typeMessage, 45);
+    }
+
+}
+
+surpriseBtn.addEventListener("click", () => {
+
+    surprise.classList.remove("hidden");
+
+    surpriseBtn.innerHTML = "🎉 Enjoy Your Day 🎉";
+
+});
